3628c3bbcc904db01269895366eadbd017c67a1d
danielronde18 <42391468+danielronde18@users.noreply.github.com>
2018-11-19 18:25:09 -0500

    Update Plots_hw4.py

--- Plots_hw4.py
+++ Plots_hw4.py
@@ -36,6 +36,20 @@ ang=[10,20,30,40,50,60,70]
 
 
 angulos=np.genfromtxt("diferentesangulos.txt")
+t=int(np.shape(angulos)[0]/len(angx))
+
+plt.figure()
+for i in range(len(angx)):
+	angulosx[i]=angulos[i*t:(i+1)*t,0]
+	angulosy[i]=angulos[i*t:(i+1)*t,1]
+	plt.plot(angx[i],angy[i],label=10*(i+1))
+plt.plot(x,y,label="45")
+plt.ylim(0,6)
+plt.title("diferentes angulos")
+plt.ylabel("y (m)")
+plt.xlabel("x (m)")
+plt.legend()
+plt.savefig("variadas.pdf")
 
 
 
