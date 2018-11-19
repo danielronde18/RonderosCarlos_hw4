ab9ff64bb7e87600111dfd79673000b78e7fe2b1
danielronde18 <42391468+danielronde18@users.noreply.github.com>
2018-11-19 18:22:27 -0500

    Update Plots_hw4.py

--- Plots_hw4.py
+++ Plots_hw4.py
@@ -30,37 +30,8 @@ plt.ylim(0,5)
 plt.savefig("45.pdf")
 
 
-angx=["x10","x20","x30","x40","x50","x60","x70"]
-angy=["y10","y20","y30","y40","y50","y60","y70"]
-ang=[10,20,30,40,50,60,70]
 
 
 
-angulos=np.genfromtxt("diferentesangulos.txt")
-
-t=int(np.shape(angulos)[0]/len(angx))
-
-plt.figure()
-for i in range(len(angx)):
-	angulosx[i]=angulos[i*t:(i+1)*t,0]
-	angulosy[i]=angulos[i*t:(i+1)*t,1]
-	plt.plot(angx[i],angy[i],label=10*(i+1))
-plt.plot(x,y,label="45")
-plt.ylim(0,6)
-plt.title("diferentes angulos")
-plt.ylabel("y (m)")
-plt.xlabel("x (m)")
-plt.legend()
-plt.savefig("variadas.pdf")
-
-
-valorMax=np.linspace(10,100,7)
-
-#con esta funcion saque los valores maximos en x, los inserte en c++
-for i in range(len(angx)):
-	aux=100
-	for j in range(1000,t):
-		if(abs(angy[i][j])<aux):
-			aux=angy[i][j]
-			valorMax[i]=angx[i][j]
+	
 
