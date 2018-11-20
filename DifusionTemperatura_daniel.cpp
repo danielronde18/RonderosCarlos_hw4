0cace8f0f994b9b24e89cc41a6c0ba5d34eb7d76
danielronde18 <42391468+danielronde18@users.noreply.github.com>
2018-11-19 20:02:55 -0500

    Update DifusionTemperatura_daniel.cpp

--- DifusionTemperatura_daniel.cpp
+++ DifusionTemperatura_daniel.cpp
@@ -15,113 +15,91 @@ using namespace std;
 
 #define N 100
 #define nu 1.0
-#define OPEN 0
-#define CLOSED 1
-#define PERIODIC 2
-#define INITIAL 0
-#define CONSTANT 1
+#define abierto 0
+#define fijo 1
+#define periodico 2
+#define l 10
 
-#define T_WHOLE 50.0
-#define T_SPOT 100.0
 
-double dx, dt,m,t[][],tpl[][];
+double dx, dt,m,y[l][l];
 
-double tpl[N][N];
-double t[N][N];
+double pre[N],fut[N];
 double coef=(k*dt)/(Cp*(dx**2));
 
-double inicializa(){
-	for(int i=1; i<N-1; i++)                 /* clear the array  */
-	{   
-		for(int j=0,j<N-1,j++){
-			
-				t[i][j] = 100.0;
-			
+
+void inicializa(){
+	for(int i=0; i<N-1; i++)              
+	{   for(int j=0; j<N-1; j++){
+		y[i][j]= 100.0;}		
+	}
 }
+
+void fronterafija(){	
+y[0][l]=10.0;
+y[l][0]=10.0;
+y[0][0]=10.0;
+y[l][l]=10.0;
 }
-}
-/*	
-    t[0][0] = 10.0;       
-    t[0][N-1] = 10.0;        
-   t[N-1][N-1]=10.0;
-t[N-1][0]=10.0;
-int m=1;*/
-/*
+
+void rellena(){
+for(i=1;i<l;i+dx){
+	pas[i]=0;
+	}}
 void ecuacionfuturo(){
 
 	for (int i =1,i<N,i++){
-		for(int j=1,j<N-1,j++){
-			t[i][j+1]=t[i][j]+(k*dt)/(Cp*(dx**2))*(t[i+1][j]+t[i-1][j]-(2*t[i][j]));
-			//fut[j+1]=pre[i]+(k*dt)/(Cp*(dx**2))*(pre[i+1]+pre[i-1]-(2*pre[i]))
-		}
-	}for (int i =1,i<N, i++){
-		for (int j=1,j<N,j++){
-		t[i][j]=t[i][j+1];
+		
+			fut[j+1]=pre[i]+(k*dt)/(Cp*(dx**2))*(pre[i+1]+pre[i-1]-(2*pre[i]))
+		
+		for (int i =1,i<N, i++){
+		
+			pre[i][j]=fut[i][j+1];
 	}	
 
 }}
 
-*/
 
 
 
-/*
-void problemainicial(double *grid)
+
+
+void seleccion_caso( int caso)
 {
-    int width = 20/dx, height_half = 5/dx;
-
-    int i, j, index;
-    int i_half = N*0.5, j_half = 0.20*N;
-
-    for(i = 0; i < N; i++)
-    {
-        for(j = 0; j < N; j++)
-        {
-            index = position(i, j);
-            grid[index] = T_WHOLE;
-            if((j < j_half + width) && (j > j_half))
-            {
-                if((i < i_half + height_half) && (i > i_half - height_half))
-                {
-                    grid[index] = T_SPOT;
-                }
-            }
-        }
+    if (caso == fijo)
+    {   std::ofstream file2;
+	file2.open("fijo.txt")
+	inicializa();
+	fronterafija();
+	rellena();
+	ecuacionfuturo();
+	for(int i=0;i<N,i++){
+	file2<<y[i][j]<<""<<pre[i]<<""<<fut[i]<<endl;
+	}
+	file2.close();
     }
-}
-
-*/
-char* seleccion_caso(int limite , int caso)
-{
-    static char almacena[100];
-    char sellim[100];
-    if (limite == CLOSED)
+    else if (caso == cerrado)
     {
-        sprintf(sellim, "cerrado");
+		        
     }
-    else if (limite == OPEN)
+    else if (caso == periodico)
     {
-        sprintf(sellim, "abierto");
+        
     }
-    else if (limite == PERIODIC)
-    {
-        sprintf(sellim, "periodico");
-    }
-    if (caso == CONSTANT)
-    {
-        sprintf(almacena, "constant_%s", sellim);
-    }
-    else
-    {
-        sprintf(almacena, "initial_%s", sellim);
-    }
-    return almacena;
+   
 }
 
 
-int main(void){
+int main(){
+
+cout<<"ingrese un numero segun sea el caso que desea resolver"<<endl;
+cin>>a;
+int *caso;
+caso=a
+seleccion_caso(*caso);
+
 
  
-
+return 0;
 }
+
 
